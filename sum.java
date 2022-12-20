b63c954c7a4f5d8d91fb229220ec8eb067723f07
jaswanthbandela162 <121084286+jaswanthbandela162@users.noreply.github.com>
2022-12-20 15:02:44 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class program
+{
+   public static void main(String args[])
+   {
+       int a,b;
+       Scanner s = new Scanner(System.in);
+       System.out.println("Enter two numbers:");
+       a = s.nextInt();
+       b = s.nextInt();
+       System.out.println("sum of two numbers:"+(a+b);
+   }
+}
